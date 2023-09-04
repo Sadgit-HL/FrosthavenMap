082ca1b126ca1910afc91b89b6555fb734f0f24a
Sadgit-HL <sadgit@penultimate.de>
2023-09-04 10:00:19 +0200

    Add py lava

--- scripts/constants.js
+++ scripts/constants.js
@@ -595,6 +595,7 @@ FAMILIARS_RAW = [
 	[18, 'oj ice pillar', 1, 1, 46, 39, bg],
 	[19, 'Treasure', 1, 1, 46, 39, bg],
 	[20, 'tr trap', 1, 1, 46, 39, bg],
+	[21, 'ha lava', 1, 1, 46, 39, bg],
 ];
 
 FAMILIARS_LIST = FromRAWToLIST(FAMILIARS_RAW);
