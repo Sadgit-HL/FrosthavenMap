f5491c5e89ce1fff000def974aa3bf699c5b463d
Sadgit-HL <sadgit@penultimate.de>
2025-11-04 11:09:53 +0100

    added mp

--- scripts/constants.js
+++ scripts/constants.js
@@ -421,6 +421,7 @@ for (var i = 0; i < LIEUTENANTS_RAW.length; i++) {
 var MAX_Heroes = 4
 
 var HEROES_RAW = [
+	[41, 'Anaphi', 1, 1, 41, 35, bg, , , dummy],
 	[1, 'Wildfury', 1, 1, 41, 35, bg, , , dummy],
 	[2, 'Banner Spear', 1, 1, 41, 35, bg, , , dummy],
 	[3, 'Berserker', 1, 1, 41, 35, bg, , , dummy],
@@ -461,7 +462,6 @@ var HEROES_RAW = [
 	[38, 'Sunkeeper', 1, 1, 41, 35, bg, , , dummy],
 	[39, 'Tinkerer', 1, 1, 41, 35, bg, , , dummy],
 	[40, 'Voidwarden', 1, 1, 41, 35, bg, , , dummy],
-	[41, 'Anaphi', 1, 1, 41, 35, bg, , , dummy],
 	[42, 'Satha', 1, 1, 41, 35, bg, , , dummy],
  ];
 
